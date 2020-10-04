482cf0121d34c0a67870007ec685fb7b28f74d8c
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-04 15:45:59 +0530

    Welcome to Regex

---
+++ src/regex/RegexPin.java
@@ -0,0 +1,12 @@
+package regex;
+import java.util.Scanner;
+import java.util.regex.*; 
+
+class RegexPin { 
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Regular Expressions");
+	
+	}
+} 
