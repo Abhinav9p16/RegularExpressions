5ac1532ccce18ad2154690562d794c56f89cade4
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-04 15:47:54 +0530

    PIN

--- src/regex/RegexPin.java
+++ src/regex/RegexPin.java
@@ -3,10 +3,24 @@ import java.util.Scanner;
 import java.util.regex.*; 
 
 class RegexPin { 
-	
+	public static boolean isValidPinCode(String pin) {
+		// Regex to check valid pin code of India.
+	   String regex = "^[1-9]{1}[0-9]{2}{0,1}[0-9]{3}$";
+
+		Pattern p = Pattern.compile(regex);
+		if (pin == null) {
+			return false;
+		}
+		Matcher m = p.matcher(pin);
+		return m.matches();
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Regular Expressions");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the PINCODE");
+		String pin = sc.nextLine();
+		System.out.println(pin + ":" + isValidPinCode(pin));
 	
 	}
 } 
