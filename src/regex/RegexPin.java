ede139b80db83c07e0ffd38dce6b003bcb1146dc
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-04 16:41:58 +0530

    Final check

--- src/regex/RegexPin.java
+++ src/regex/RegexPin.java
@@ -1,11 +1,12 @@
 package regex;
-import java.util.Scanner;
-import java.util.regex.*; 
 
-class RegexPin { 
+import java.util.Scanner;
+import java.util.regex.*;
+
+class RegexPin {
 	public static boolean isValidPinCode(String pin) {
 		// Regex to check valid pin code of India.
-	   String regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
+		String regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
 
 		Pattern p = Pattern.compile(regex);
 		if (pin == null) {
@@ -14,9 +15,10 @@ class RegexPin {
 		Matcher m = p.matcher(pin);
 		return m.matches();
 	}
+
 	public static boolean isValidName(String fname) {
 		// Regex to check valid username of India.
-	   String regex = " ^[A-Z]{1}[a-zA-Z]{2,}$";
+		String regex = " ^[A-Z]{1}[a-zA-Z]{2,}$";
 
 		Pattern p = Pattern.compile(regex);
 		if (fname == null) {
@@ -25,9 +27,10 @@ class RegexPin {
 		Matcher m = p.matcher(fname);
 		return m.matches();
 	}
+
 	public static boolean isValidPHno(String num) {
 		// Regex to check valid pin code of India.
-	   String regex = "^(91)\\s{1}[1-9]{1}[0-9]{9}$";
+		String regex = "^(91)\\s{1}[1-9]{1}[0-9]{9}$";
 
 		Pattern p = Pattern.compile(regex);
 		if (num == null) {
@@ -36,9 +39,11 @@ class RegexPin {
 		Matcher m = p.matcher(num);
 		return m.matches();
 	}
+
 	public static boolean isValidEmail(String email) {
 		// Regex to check valid email of India.
-		String regex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+){0,1}(\\.[A-Za-z]{2,})$";
+		String regex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+				+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+){0,1}(\\.[A-Za-z]{2,})$";
 		Pattern p = Pattern.compile(regex);
 		if (email == null) {
 			return false;
@@ -46,8 +51,9 @@ class RegexPin {
 		Matcher m = p.matcher(email);
 		return m.matches();
 	}
+
 	public static boolean isValidPass(String pass) {
-		// Regex to check valid pin code of India.
+		// Regex to check valid password of India.
 		String regex = "^(?=.*[0-9])" + "(?=.*[a-z])(?=.*[A-Z])" + "(?=.*[@#$%^&+=])" + "(?=\\S+$).{8,}$";
 
 		Pattern p = Pattern.compile(regex);
@@ -57,27 +63,33 @@ class RegexPin {
 		Matcher m = p.matcher(pass);
 		return m.matches();
 	}
-	public static void main(String[] args) {
-		System.out.println("Welcome to Regular Expressions");
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the PINCODE");
-		String pin = sc.nextLine();
-		System.out.println(pin + ":" + isValidPinCode(pin));
-		System.out.println("Enter First name ");
-		String fname = sc.nextLine();
-		System.out.println(fname + ":" + isValidName(fname));
-		System.out.println("Enter Last name ");
-		String lname = sc.nextLine();
-		System.out.println(lname + ":" + isValidName(lname));
-		System.out.println("Enter Phone number ");
-		String phno = sc.nextLine();
-		System.out.println(phno + ":" + isValidPHno(phno));
-		System.out.println("Enter the EMAIL");
-		String email = sc.nextLine();
-		System.out.println(email + ":" + isValidEmail(email));
-		System.out.println("Enter Password ");
-		String pass = sc.nextLine();
-		System.out.println(pass + ":" + isValidPHno(pass));
 
+	public static void main(String[] args) {
+		while (true) {
+			System.out.println("Welcome to Regular Expressions");
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the PINCODE");
+			String pin = sc.nextLine();
+			System.out.println(pin + ":" + isValidPinCode(pin));
+			System.out.println("Enter First name ");
+			String fname = sc.nextLine();
+			System.out.println(fname + ":" + isValidName(fname));
+			System.out.println("Enter Last name ");
+			String lname = sc.nextLine();
+			System.out.println(lname + ":" + isValidName(lname));
+			System.out.println("Enter Phone number ");
+			String phno = sc.nextLine();
+			System.out.println(phno + ":" + isValidPHno(phno));
+			System.out.println("Enter the EMAIL");
+			String email = sc.nextLine();
+			System.out.println(email + ":" + isValidEmail(email));
+			System.out.println("Enter Password ");
+			String pass = sc.nextLine();
+			System.out.println(pass + ":" + isValidPass(pass));
+			System.out.println("Do you want to continue Y/N");
+			char choice = Character.toUpperCase(sc.next().charAt(0));
+			if (choice != 'Y')
+				break;
+		}
 	}
-} 
+}
