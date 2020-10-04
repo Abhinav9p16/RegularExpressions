97949c32d903c9fd4075b7912716b52260f5e8f8
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-04 23:14:06 +0530

    Exactly 1 Spec Char

--- src/regex/RegexPin.java
+++ src/regex/RegexPin.java
@@ -16,15 +16,15 @@ class RegexPin {
 		return m.matches();
 	}
 
-	public static boolean isValidName(String fname) {
-		// Regex to check valid username of India.
-		String regex = " ^[A-Z]{1}[a-zA-Z]{2,}$";
+	public static boolean isValidName(String name) {
+		// Regex to check valid username
+		String regex = "^[A-Z][a-zA-Z]{2,}$";
 
 		Pattern p = Pattern.compile(regex);
-		if (fname == null) {
+		if (name == null) {
 			return false;
 		}
-		Matcher m = p.matcher(fname);
+		Matcher m = p.matcher(name);
 		return m.matches();
 	}
 
@@ -41,7 +41,7 @@ class RegexPin {
 	}
 
 	public static boolean isValidEmail(String email) {
-		// Regex to check valid email of India.
+		// Regex to check valid email
 		String regex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
 				+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+){0,1}(\\.[A-Za-z]{2,})$";
 		Pattern p = Pattern.compile(regex);
@@ -53,9 +53,9 @@ class RegexPin {
 	}
 
 	public static boolean isValidPass(String pass) {
-		// Regex to check valid password of India.
-		String regex = "^(?=.*[0-9])" + "(?=.*[a-z])(?=.*[A-Z])" + "(?=.*[@#$%^&+=])" + "(?=\\S+$).{8,}$";
-
+		// Regex to check valid password
+		String regex = "^(?=.*[0-9])" + "(?=.*[a-z])(?=.*[A-Z])" + "(?!.*[<>`])"
+				+ "(?=[^@#$%^&+=]*[@#$%^&+=][^@#$%^&+=]*$)" + ".{8,}$";
 		Pattern p = Pattern.compile(regex);
 		if (pass == null) {
 			return false;
